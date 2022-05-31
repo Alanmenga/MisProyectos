f1012d9dddc4c25abb6674cb8f9c2f802ddca0bd
Alanmenga <alan.gabriel.mengassini@gmail.com>
2022-05-31 00:40:06 -0300

    Falto eso

--- Angular/12-mapas/src/app/components/mapa/mapa.component.ts
+++ Angular/12-mapas/src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marcador } from 'src/app/classes/marcador.class';
 
 @Component({
   selector: 'app-mapa',
@@ -6,13 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mapa.component.css']
 })
 export class MapaComponent implements OnInit {
+  
+  marcadores: Marcador[] = [];
 
   lat = 51.678418;
   lng = 7.809007;
 
-  constructor() { }
+  constructor() {
+    
+    const nuevoMarcador = new Marcador( 51.678418, 7.809007 );
+    this.marcadores.push( nuevoMarcador );
+
+  }
 
   ngOnInit(): void {
+  }
+
+  agregarMarcador( evento: any ) {
+    console.log( evento  );
   }
 
 }
